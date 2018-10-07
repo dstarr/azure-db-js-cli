9e592cc596154ef6805a0c9c373540133d015e15
david <david@elegantcode.com>
2018-10-07 10:15:37 -0700

    getting a DB and a container works.

--- app.js
+++ app.js
@@ -1,38 +1,53 @@
 let config = require("./config");
 
-let documentClient = require("documentdb").DocumentClient;
-let uriFactory = require('documentdb').UriFactory;
-let client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+let {CosmosClient} = require("@azure/cosmos");
+// let uriFactory = require('documentdb').UriFactory;
 
-var HttpStatusCodes = { NOTFOUND: 404 };
-var databaseId = config.database.id;
-var collectionId = config.collection.id;
+const masterKey = config.primaryKey;
+
+
+const client = new CosmosClient({endpoint: config.endpoint, auth: {masterKey: masterKey}});
+
+const HttpStatusCodes = {NOTFOUND: 404};
+const databaseId = config.database.id;
+const containerId = config.container.id;
+
+
+const database = getDatabase();
+const container = getContainer();
+
+// .then(() => insertDocuments())
+// //.then(() => getToDoDoc(config.documents.groceries))
+//   .then(() => {
+//     console.log(`Completed successfully`)
+//   })
+//   .catch((error) => {
+//     console.log(`Completed with error ${JSON.stringify(error)}`)
+//   });
+
 
 function getDatabase() {
 
-  console.log(`Getting database:\n${databaseId}\n`);
+  console.log(`Getting database: ${databaseId}`);
 
-  const databaseUrl = uriFactory.createDatabaseUri(databaseId);
+  return client.database(databaseId)
 
-  return new Promise((resolve, reject) => {
-
-    client.readDatabase(databaseUrl, (err, result) => {
-      if (err) {
-        console.log(err.type);
-        reject(err);
-
-      } else {
-        resolve(result);
-      }
-    });
-  });
 }
+
+async function getContainer() {
+
+  console.log(`Getting container: ${containerId}`);
+
+  const {body: container} = await database.container(containerId).read();
+  return container;
+
+};
 
 function getToDoDoc(document) {
 
-  console.log(`Getting document:\n${document.id}\n`);
+  console.log(`Getting document: ${document.id}`);
 
-  let documentUrl = uriFactory.createDocumentUri(databaseId, collectionId, document.id);
+  let documentUrl = uriFactory.createDocumentUri(databaseId, containerId, document);
 
   return new Promise((resolve, reject) => {
 
@@ -41,7 +56,7 @@ function getToDoDoc(document) {
       if (err) {
         if (err.code == HttpStatusCodes.NOTFOUND) {
 
-          let collectionUrl = uriFactory.createDocumentCollectionUri(databaseId, collectionId);
+          let collectionUrl = uriFactory.createDocumentCollectionUri(databaseId, containerId);
 
           client.createDocument(collectionUrl, document, (err, created) => {
             if (err) reject(err)
@@ -58,38 +73,31 @@ function getToDoDoc(document) {
       }
     });
   });
+
+
 };
 
-function getCollection() {
-  console.log(`Getting collection:\n${collectionId}\n`);
-  let collectionUrl = uriFactory.createDocumentCollectionUri(databaseId, collectionId);
-  return new Promise((resolve, reject) => {
-    client.readCollection(collectionUrl, (err, result) => {
-      if (err) {
-        if (err.code == HttpStatusCodes.NOTFOUND) {
-          let databaseUrl = uriFactory.createDatabaseUri(databaseId);
-          client.createCollection(databaseUrl, { id: collectionId }, { offerThroughput: 400 }, (err, created) => {
-            if (err) reject(err)
-            else resolve(created);
-          });
-        } else {
-          reject(err);
-        }
-      } else {
-        resolve(result);
-      }
+function insertDocuments() {
+
+  console.log('Inserting documents');
+
+  try {
+
+    config.documents.forEach(async (currentDoc) => {
+
+      console.log('==================== INSERT ====================');
+      console.log(currentDoc.id);
+
+      const {item: item} = await container.items.create(currentDoc);
+
+      console.log(item);
+
     });
-  });
-};
 
-getDatabase()
-  .then(() => getCollection())
-  .then(() => getToDoDoc(config.documents.groceries1))
-  .then(() => getToDoDoc(config.documents.groceries1))
-  .then(() => {
-    console.log(`Completed successfully`)
-  })
-  .catch((error) => {
-    console.log(`Completed with error ${JSON.stringify(error)}`)
-  });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+
 
--- config.js
+++ config.js
@@ -1,34 +1,34 @@
+const uuid = require('uuid/v4');
+
 let config = {};
 
-config.endpoint = "https://node-cosmos.documents.azure.com:443/";
-config.primaryKey = "1bDa0iCNSkpmHRrxtyvQRBsRIrJsSJzcA5kpixy0sTRdRM5cqAk0yLHStcORYC2NCR6EGIh272GJTJfIZBUi7g==";
+config.endpoint = "https://cli-test.documents.azure.com:443/";
+config.primaryKey = "qr1g4JbcrWv7Cc6oreNPWGyaHc4F6hnGAyfJSofbZ2sQz99RDM6kimKRkldPwFqjP9nedf8brxnHvh5Jw6qOYw==";
 
 config.database = {
-  "id": "ToDoList"
+  "id": "cli-test"
 };
 
-config.collection = {
-  "id": "Items"
+config.container = {
+  "id": "items"
 };
 
-config.documents = {
-
-  "groceries1": {
-    "id" : "id3",
-    "category" : "personal",
-    "name" : "groceries",
-    "description" : "Pick up kiwi fruit.",
-    "isComplete"  : false
-  },
-  "groceries2": {
-    "id" : "id4",
-    "category" : "personal",
-    "name" : "groceries",
-    "description" : "Pick up orange fruit.",
-    "isComplete"  : false
-  }
-};
-
-
+config.documents =
+  [
+    {
+      "id": String(uuid()),
+      "category": "personal",
+      "name": "groceries",
+      "description": "Pick up kiwi fruit.",
+      "isComplete": "false"
+    },
+    {
+      "id": String(uuid()),
+      "category": "personal",
+      "name": "groceries",
+      "description": "Pick up kiwi fruit.",
+      "isComplete": "false"
+    }
+  ];
 
 module.exports = config;
