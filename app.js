93f4fb4cc7aa719019de80ef7899640fdcf9587a
david <david@elegantcode.com>
2018-10-21 09:38:37 -0700

    sample operations working

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const config = require("./config");
 const CosmosClient = require("@azure/cosmos").CosmosClient;
-
+const config = require("./config");
+const uuid = require('uuid');
 const masterKey = config.primaryKey;
-
-const client = new CosmosClient({endpoint: config.endpoint, auth: {masterKey: masterKey}});
 
 const databaseId = config.database.id;
 const containerId = config.container.id;
+
+const client = new CosmosClient({endpoint: config.endpoint, auth: {masterKey: masterKey}});
 
 // The addNewDocs executes on it's own thread so quickly the deleteAllDocs
 // runs before before insert completes.
 // This means the delete doesn't typically actually delete all the docs.
 // This means designing for eventual consistenty
-addNewDocs()
-  .then(() => deleteAllDocs())
+// addNewDocs()
+//   .then(() => deleteAllDocs())
+deleteAllDocs()
   .then(() => listAll())
   .catch((err) => {
     if (err)
@@ -22,20 +23,28 @@ addNewDocs()
 
 async function addNewDocs() {
 
-  config.documents.forEach((doc) => {
-    addItem((doc))
+  for (i = 0; i < 100; i++) {
+
+    const id = uuid();
+
+    let doc = {id: id, content: `The id of this document is: ${id}`};
+
+    await addItem(doc)
+      .then(() => {
+        console.log(`${doc.id} added`);
+      })
       .catch((err => {
         console.error(err);
       }));
+  }
 
-    console.log(`-------- INSERTED ${doc.id}`);
-  });
 }
 
 async function addItem(item) {
 
   const {database} = await client.databases.createIfNotExists({id: databaseId});
   const {container} = await database.containers.createIfNotExists({id: containerId});
+
   await container.items.create(item);
 }
 
@@ -60,9 +69,11 @@ async function listAll() {
   const {database} = await client.databases.createIfNotExists({id: databaseId});
   const {container} = await database.containers.createIfNotExists({id: containerId});
 
-  const { result: allDocs } = await container.items.readAll().toArray();
+  const {result: allDocs} = await container.items.readAll().toArray();
 
   allDocs.map((doc) => {
     console.log(`DOC IN DB: ${doc.id}`);
   });
 }
+
+
--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const uuid = require('uuid/v4');
 
 let config = {};
 
-config.endpoint = "https://<prefix>.documents.azure.com:443/";
-config.primaryKey = "<my primary key>";
+config.endpoint = "https://cli-test.documents.azure.com:443/";
+config.primaryKey = "qr1g4JbcrWv7Cc6oreNPWGyaHc4F6hnGAyfJSofbZ2sQz99RDM6kimKRkldPwFqjP9nedf8brxnHvh5Jw6qOYw==";
 
 config.database = {
   "id": "cli-test"
@@ -12,19 +12,5 @@ config.database = {
 config.container = {
   "id": "items"
 };
-
-config.documents =
-  [
-    {
-      "id": String(uuid()),
-      "description": "Pick up a coconut.",
-      "isComplete": "false"
-    },
-    {
-      "id": String(uuid()),
-      "description": "Pick up kiwi fruit.",
-      "isComplete": "false"
-    }
-  ];
 
 module.exports = config;
