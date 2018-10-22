3d0adfbd970e97b320f146670b86985df6aea646
david <david@elegantcode.com>
2018-10-21 19:53:06 -0700

    got sinel ads and deletes working with queries.

--- app.js
+++ app.js
@@ -8,35 +8,68 @@ const containerId = config.container.id;
 
 const client = new CosmosClient({endpoint: config.endpoint, auth: {masterKey: masterKey}});
 
+let docId = null;
+
 // The addNewDocs executes on it's own thread so quickly the deleteAllDocs
-// runs before before insert completes.
-// This means the delete doesn't typically actually delete all the docs.
 // This means designing for eventual consistenty
-// addNewDocs()
-//   .then(() => deleteAllDocs())
-deleteAllDocs()
-  .then(() => listAll())
+addNewDocs()
+  //.then(() => listAll())
+  //.then(() => deleteAllDocs())
+  .then(() => addOneDoc())
+  .then(() => fetchOneDocument())
   .catch((err) => {
     if (err)
       console.error(err);
   });
 
+async function fetchOneDocument() {
+
+  console.log(`FETCHING: ${docId}`);
+
+  const query = `select * from i where i.id = '${docId}'`;
+
+  const {database} = await client.databases.createIfNotExists({id: databaseId});
+  const {container} = await database.containers.createIfNotExists({id: containerId});
+  const {result: items} = await container.items.query(query).toArray();
+
+  if (items.length !== 1)
+    console.error(`GOT ${items.length} BACK`);
+  else
+    console.log(`RECEIVED: ${docId}`);
+}
+
+// This means the delete doesn't typically actually delete all the docs.
+async function addOneDoc() {
+
+  docId = uuid();
+
+  const doc = {
+    id: docId,
+    content: "Would that I had another lifetime to spend with her in love."
+  };
+
+  await addItem(doc);
+
+}
+
 async function addNewDocs() {
 
+  const {database} = await client.databases.createIfNotExists({id: databaseId});
+  const {container} = await database.containers.createIfNotExists({id: containerId});
+
+  const p = [];
+
+  console.log("INSERTING 100 DOCS");
   for (i = 0; i < 100; i++) {
 
     const id = uuid();
-
     let doc = {id: id, content: `The id of this document is: ${id}`};
-
-    await addItem(doc)
-      .then(() => {
-        console.log(`${doc.id} added`);
-      })
-      .catch((err => {
-        console.error(err);
-      }));
+    p.push(container.items.create(doc));
   }
+
+  await Promise.all(p);
+  console.log("DONE INSERTING 100 DOCS");
+
 
 }
 
@@ -45,6 +78,7 @@ async function addItem(item) {
   const {database} = await client.databases.createIfNotExists({id: databaseId});
   const {container} = await database.containers.createIfNotExists({id: containerId});
 
+  console.log("ADDING: " + item.id);
   await container.items.create(item);
 }
 
